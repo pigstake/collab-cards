472cda91924c29ba6e1a809d392c89f1358add5b
Jamie Caloras <pigstake@users.noreply.github.com>
2020-05-21 14:13:35 -0400

    working fairplay button

--- src/PromptAndDetails.js
+++ src/PromptAndDetails.js
@@ -9,11 +9,25 @@ class PromptAndDetails extends React.Component {
     return (
       <div className="promptContainer">
         <button
-          className="newPromptButton gradientFlow"
+          className="button newPromptButton gradientFlow hoverTransition"
           onClick={this.props.handleClick}
         >
           Deal a Collab Card
         </button>
+        <label htmlFor="fairPlayButton" className="button fairPlayButton hoverTransition">;)</label>
+        <input type="checkbox" id="fairPlayButton" className="fairPlayButton-input"></input>
+        <div className="fairPlay">
+          <div className="fairPlay-content">
+            Inspired by www.trytriggers.com, Collab Cards are designed to provoke thought and highlight core values during your meeting at TeachersPayTeachers.
+            You can use them to widen your view, to narrow your concept, to expand your perspective, or to challenge your norms. Or just to break the ice.
+            These cards have been intentionally designed to incorporate TpT values. In line we these values, we'd like to remind you to:
+            - listen first
+            - challenge ideas respectfully
+            - ask to understand
+            - speak to each other with respect
+            Lastly, we recommend that you take turns doing the flipping. This will give quieter voices a chance to enter the conversation and newer voices a chance to lead.
+          </div>
+        </div>
         <div
           className="fadeIn prompt"
           key={Math.random()}
